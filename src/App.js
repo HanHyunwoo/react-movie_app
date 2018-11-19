ebac5b56426a8c2a54d6dcfc7569f0310255e1c1
HOME <hhw1990@naver.com>
2018-11-20 02:23:16 +0900

    map을 이용하여 리스트 생성

--- src/App.js
+++ src/App.js
@@ -2,29 +2,63 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "Matrix",
-  "Full Metal Jacket",
-  "Oldboy",
-  "Star Wars"
-]
+// const movieTitles = [
+//   "Matrix",
+//   "Full Metal Jacket",
+//   "Oldboy",
+//   "Star Wars"
+// ]
 
-const movieImages = [
-  "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
-  "https://images-na.ssl-images-amazon.com/images/I/41MN0ANVJTL.jpg",
-  "https://www.nautiljon.com/images/asian-movie/00/23/old_boy_132.jpg",
-  "https://vignette.wikia.nocookie.net/starwars/images/c/c5/Lego-all-stars-promo.jpg/revision/latest?cb=20181026172220"
-]
+// const movieImages = [
+//   "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+//   "https://images-na.ssl-images-amazon.com/images/I/41MN0ANVJTL.jpg",
+//   "https://www.nautiljon.com/images/asian-movie/00/23/old_boy_132.jpg",
+//   "https://vignette.wikia.nocookie.net/starwars/images/c/c5/Lego-all-stars-promo.jpg/revision/latest?cb=20181026172220"
+// ]
 
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg"
+  },
+  {
+    title: "Full Metal Jacket",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/41MN0ANVJTL.jpg"
+  },
+  {
+    title: "Oldboy",
+    poster: "https://www.nautiljon.com/images/asian-movie/00/23/old_boy_132.jpg"
+  },
+  {
+    title: "Star Wars",
+    poster: "https://vignette.wikia.nocookie.net/starwars/images/c/c5/Lego-all-stars-promo.jpg/revision/latest?cb=20181026172220"
+  }
+
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
+         {/*
          <Movie title={movieTitles[0]} poster={movieImages[0]} />
          <Movie title={movieTitles[1]} poster={movieImages[1]} />
          <Movie title={movieTitles[2]} poster={movieImages[2]} />
          <Movie title={movieTitles[3]} poster={movieImages[3]} />
+         */}
+
+         {movies.map(movie =>{
+           return <Movie title={movie.title} poster={movie.poster} />
+         })}
+
+         {/*
+         <Movie title={movies[0].title} poster={movies[0].poster} />
+         <Movie title={movies[1].title} poster={movies[1].poster} />
+         <Movie title={movies[2].title} poster={movies[2].poster} />
+         <Movie title={movies[3].title} poster={movies[3].poster} />
+         */}
+
       </div>
     );
   }
