3db0c870146e0c20543c7b98d6f391d2b287a193
HOME <hhw1990@naver.com>
2018-11-20 01:16:52 +0900

    props 데이터 흐름

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
+const movieTitles = [
+  "Matrix",
+  "Full Metal Jacket",
+  "Oldboy",
+  "Star Wars"
+]
+
+const movieImages = [
+  "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/41MN0ANVJTL.jpg",
+  "https://www.nautiljon.com/images/asian-movie/00/23/old_boy_132.jpg",
+  "https://vignette.wikia.nocookie.net/starwars/images/c/c5/Lego-all-stars-promo.jpg/revision/latest?cb=20181026172220"
+]
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-         <Movie />
-         <Movie />
-         <Movie />
-         <Movie />
+         <Movie title={movieTitles[0]} poster={movieImages[0]} />
+         <Movie title={movieTitles[1]} poster={movieImages[1]} />
+         <Movie title={movieTitles[2]} poster={movieImages[2]} />
+         <Movie title={movieTitles[3]} poster={movieImages[3]} />
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -3,10 +3,12 @@ import './Movie.css';
 
 class Movie extends Component{
   render(){
+    {/*console.log(this.props); */}
+
     return(
       <div>
-        <MoviePoster />
-        <h1>hello this is a movie</h1>
+        <MoviePoster poster={this.props.poster}/>
+        <h1>{this.props.title}</h1> { /*JSX의 경우 명령을 실행시키려면 중괄호를 꼭 쳐야한다. */}
       </div>
     )
   }
@@ -14,8 +16,9 @@ class Movie extends Component{
 
 class MoviePoster extends Component{
   render(){
+    console.log(this.props);
     return(
-      <img src="http://blogpfthumb.phinf.naver.net/MjAxNzEwMDhfMjE2/MDAxNTA3NDA4MzMzNzQw.7Vml0qN_E3U_5g3ABJUWIIoeM3Abs3qwZ77qbKBNHysg.Azn5caoiQiD51kJp90xyJMFi5YVlRiKkyCCLu28-hJsg.JPEG.hhw1990/%BB%E7%C1%F8.jpg?type=w161" />
+      <img src={this.props.poster} />
     )
   }
 }
