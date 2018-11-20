50a17d262d43d95fadd47c43a2421509a794e0ea
HOME <hhw1990@naver.com>
2018-11-21 04:10:37 +0900

    컴포넌트 Lifecycle 개념추가

--- src/App.js
+++ src/App.js
@@ -37,7 +37,38 @@ const movies = [
 ]
 
 class App extends Component {
+
+  // Render : componentWillMount() -> render() -> componentDidMount()
+  // Update : componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+
+  // 컴포넌트가 새로운 props를 받았다.
+  componentWillReceiveProps(){}
+
+  // 리액트는 old props, 새로운 props를 살펴보고 props가 다르면 리액트는 '업데이트==true'라고 생각한다.
+  shouldComponentUpdate(){}
+
+  // 업데이트를 한다. 예를 들어 어플리케이션에 '빙긍빙글 돌아가는 spinner'를 붙일 수가 있다.
+  componentWillUpdate(){}
+
+  // 업데이트 이후에는 돌고 있던 '로딩 중'메시지나 아이콘을 숨긴다.
+  componentDidUpdate(){}
+
+
+//----------------------------------------------------------------------------------
+
+  // 컴포넌트 싸이클이 시작되었다
+  componentWillMount(){
+    console.log('will mount')
+  }
+
+  // 컴포넌트가 성공적으로 리액트 세계에 자리 잡았음을 알게되었다.
+  componentDidMount(){
+    console.log('did mount')
+  }
+
+  // 컴포넌트가 리액트 세계에 '존재'하게 되었음을 알게되었다.
   render() {
+    console.log('did render')
     return (
       <div className="App">
 
